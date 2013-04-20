928afb09b1d9b33c4bcfbd3555c109f5bb5fe335
Vipul Anand <anandvip@gmail.com>
2013-04-21 01:53:04 +0530

    Create car.js
    12 liner js

---
+++ car.js
@@ -0,0 +1,12 @@
+// JavaScript Document
+function showcar()
+{
+  var myCar = "Alto K10";
+		document.getElementById('thecar').innerHTML=myCar;
+	var isSmooth = "WOW! This is Awesome!"
+		document.getElementById('amazeme').innerHTML=isSmooth;
+	var hdr = "It is Definitely";
+		document.getElementById('wow').innerHTML=hdr;
+
+}
+console.log("Go For it");
